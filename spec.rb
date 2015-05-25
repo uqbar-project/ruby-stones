223f93fcc342d4daa2012dce6bfa9f028ade98c0
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-25 17:04:27 -0300

    Adding program

--- spec.rb
+++ spec.rb
@@ -34,15 +34,59 @@ module Gobgems
   def black; Expression.new('Negro'); end
 
   def pop(color)
-    Expression.new("Sacar(#{color.compile})")
+    clean_context { pop(color )}
   end
 
   def push(color)
-    Expression.new("Poner(#{color.compile})")
+    clean_context { push(color) }
   end
 
   def move(direction)
-    Expression.new("Mover(#{direction.compile})")
+    clean_context { move(direction) }
+  end
+
+
+  def program(&block)
+    context = Context.new('  ')
+    context.instance_eval &block
+    Expression.new("program {\n#{context.compile}}")
+  end
+
+  private
+
+  def clean_context(&block)
+    c = Context.new
+    c.instance_eval(&block)
+  end
+
+  class Context
+    def initialize(tab='')
+      @actions = []
+      @tab = tab
+    end
+
+    def pop(color)
+      add_action Expression.new("Sacar(#{color.compile})")
+    end
+
+    def push(color)
+      add_action Expression.new("Poner(#{color.compile})")
+    end
+
+    def move(direction)
+      add_action Expression.new("Mover(#{direction.compile})")
+    end
+
+    def compile
+      @actions.map { |a| "#{@tab}#{a.compile}\n" }.join('')
+    end
+
+    private
+
+    def add_action(action)
+      @actions << action
+      action
+    end
   end
 end
 
@@ -80,5 +124,15 @@ describe "valores" do
 
     it { expect((pop red).compile).to eq 'Sacar(Rojo)' }
     it { expect((pop green).compile).to eq 'Sacar(Verde)' }
+  end
+
+  describe "program" do
+    it { expect((program do end).compile).to eq "program {\n}" }
+    it { expect((program do true end).compile).to eq "program {\n}" }
+    it { expect((program do 2 end).compile).to eq "program {\n}" }
+    it { expect((program do red end).compile).to eq "program {\n}" }
+    it { expect((program do green end).compile).to eq "program {\n}" }
+    it { expect((program do push red end).compile).to eq "program {\n  Poner(Rojo)\n}" }
+    it { expect((program do push red; push green end).compile).to eq "program {\n  Poner(Rojo)\n  Poner(Verde)\n}" }
   end
 end
